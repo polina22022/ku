1180a10b852ace2c5dd332fca08630e7602c1942
Polina Medvedeva <polinamed.22.02@mail.ru>
2025-11-11 14:12:38 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -67,3 +67,79 @@ int main() {
 	}
 
 }
+
+
+
+после занятия 
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include <math.h>
+#include <locale.h>
+
+double fa(double x);
+
+int main() {
+	int choice;
+	setlocale(LC_ALL, "RUS");
+	double x;
+	double a, b;
+	int step;
+	printf("\nВыберите операцию:\n");
+	printf("1. Значение в точке\n");
+	printf("2. Таблица значений\n");
+	printf("3. Вычисление интеграла\n");
+	printf("4. Поиск x при заданном Y\n");
+	printf("5. Вторая производная в точке\n");
+	printf("0. Выход\n");
+	printf("Ваш выбор: ");
+	scanf("%d", &choice);
+
+
+	switch (choice) {
+	case 1:
+		printf("Введите x: ");
+		scanf("%lf", &x);
+		printf("f(%.4f) = %.6f\n", x, fa(x));
+		break;
+	case 2:
+		printf("Введите интервал [a, b]:\n");
+		printf("a = ");
+		scanf("%lf", &a);
+		printf("b = ");
+		scanf("%lf", &b);
+		printf("Введите шаг для таблицы (например, 1): ");
+		scanf("%d", &step);
+		break;
+
+
+	}
+
+}
+
+double fa(double x) {
+	double y;
+	double sum = 0;
+	if (x < -1) {
+		y = 1 / sqrt(1 - x);
+	}
+	else if (x >= -1 && x < 0) {
+		y = tanh(pow(x, 2));
+	}
+	else if (x > 0 && x < 2) {
+		y = log(1 + 2 * x) / x;
+	}
+	else {
+		for (int n = 0; n <= 6; ++n)
+		{
+			double k = pow(x, 2 * n);
+			long long factorial = 1;
+			for (int i = 1; i <= 2 * n; i++) {
+				factorial *= i;
+			}
+			double d = factorial * (n + 1);
+			sum += k / d;
+		}
+		y = sum;
+	}
+	return y;
+}
